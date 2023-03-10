14497f475f7fb3d7ff0a795683b8505b010201d8
joao.belmiro <joao.belmiro@valemobi.com.br>
2023-03-10 17:48:35 -0300

    Initial Commit

---
+++ Laser/LaserApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaserApp.swift
+//  Laser
+//
+//  Created by Joao Belmiro on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LaserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
